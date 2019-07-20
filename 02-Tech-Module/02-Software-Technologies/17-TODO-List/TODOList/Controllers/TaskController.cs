ef3ef80b3a93754c6e4df34f9e74b0b667c83d0b
plamenpik <plamen_pik@abv.bg>
2019-07-20 17:13:47 +0300

    Tech Module May 2018

---
+++ 02-Tech-Module/02-Software-Technologies/17-TODO-List/TODOList/Controllers/TaskController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TODOList.Models;
+
+namespace TODOList.Controllers
+{
+    public class TaskController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create(Models.Task task)
+        {
+            if (task == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            using (var db = new TaskDBContext())
+            {
+                db.Tasks.Add(task);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            using (var db = new TaskDBContext())
+            {
+                var task = db.Tasks.Find(id);
+
+                if (id == null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                db.Tasks.Remove(task);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ 02-Tech-Module/02-Software-Technologies/17-TODO-List/TODOList/Models/TaskDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TODOList.Models
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class TaskDBContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        public TaskDBContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=todo;user=root;SSLMode=none");
+        }
+    }
+}
