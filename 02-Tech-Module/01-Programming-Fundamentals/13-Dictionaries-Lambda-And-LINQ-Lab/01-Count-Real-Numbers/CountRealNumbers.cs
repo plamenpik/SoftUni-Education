903b5963ae50de0fd8f8f28778cc45b6773a0ef0
plamenpik <plamen_pik@abv.bg>
2019-07-20 22:43:24 +0300

    Tech Module May 2018

--- 02-Tech-Module/01-Programming-Fundamentals/13-Dictionaries-Lambda-And-LINQ-Lab/01-Count-Real-Numbers/CountRealNumbers.cs
+++ 02-Tech-Module/01-Programming-Fundamentals/13-Dictionaries-Lambda-And-LINQ-Lab/01-Count-Real-Numbers/CountRealNumbers.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-class CountRealNumbers
+namespace Problem_1_Count_Real_Numbers
 {
-    static void Main()
+    class Program
     {
-       
+        static void Main(string[] args)
+        {
+            var numbers = new SortedDictionary<double, int>();
+            var input = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (!numbers.ContainsKey(input[i]))
+                {
+                    numbers[input[i]] = 1;
+                }
+                else
+                {
+                    numbers[input[i]]++;
+                }
+            }
+            foreach (var kvp in numbers)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+
+        }
     }
 }
